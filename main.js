6e744f436adb8506406ad1f56e7a54116b83fe71
iL0g1c <mathtodd05@gmail.com>
2022-10-30 12:29:20 -0400

    hello

---
+++ main.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Air-Superiority Script
+// @namespace    http://tampermonkey.net/
+// @version      5.0
+// @description  Follow Other GeoFS Players Automatically
+// @author       Colonel Nichola Joseph "Nick" Fury, S.H.I.E.L.D  R & D - GeoFS MRP (UN)
+// @match        https://www.geo-fs.com/geofs.php
+// @match        https://www.legacy.geo-fs.com/geofs.php
+// @icon         https://www.google.com/s2/favicons?domain=geo-fs.com
+// @grant        none
+// ==/UserScript==
+(function() {
+    function mainLoad() {
+        
+    }
+})();
